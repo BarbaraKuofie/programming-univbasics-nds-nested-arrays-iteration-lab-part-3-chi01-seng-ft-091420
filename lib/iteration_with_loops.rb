7c25cd16defe62481647af7fcd28bcdc66b488ae
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-29 04:36:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,4 +8,8 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do 
       if src[row_index][element_index] = string 
         single_value += src[row_index][element_index]
+      end 
+      element_index += 1 
+    end 
+    
 end
